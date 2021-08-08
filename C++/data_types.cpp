1467d047133bddcde252f91c8da6fca1cc709282
Mahe Karim <mahekarim@deligram.com>
2021-08-09 00:22:27 +0600

    Data Types

---
+++ C++/data_types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    char grade = 'A';
+    string phrase = "Giraffe Academy";
+    int age = 50 ;
+    float moneyInBDT = 13.66;  // double
+    bool isValid = true ;
+
+    cout << "Hi! " << endl;
+
+
+    return 0;
+}
