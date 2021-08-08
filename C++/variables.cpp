58debbe0f906049e81df04ac9dfffe29a5311906
Mahe Karim <mahekarim@deligram.com>
2021-08-09 00:14:55 +0600

    Variables

---
+++ C++/variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+    using namespace std;
+
+    int main() {
+
+        string charName = "Mahe Karim" ;
+        int charAge = 35;
+
+
+        cout << "There is " << charName << endl;
+        cout << "He is " << charAge << " years old" << endl;
+
+        charName = "New Mahe";
+        cout << "His new name is "<< charName << endl;
+
+        return 0;
+    }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Interview_Board
+
+# I am going to solve this problem set https://drive.google.com/file/d/1FMdN_OCfOI0iAeDlqswCiC2DZzD4nPsb/view
